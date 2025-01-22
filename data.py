569a7e578b062c9f664d9b812eaf8c39748ad8f5
MightContainNuts <mightcontainnuts@icloud.com>
2025-01-22 06:22:50 +0100

    add getting light number details

---
+++ data.py
@@ -0,0 +1,60 @@
+from sqlalchemy import create_engine, text
+
+QUERY_FLIGHT_BY_ID = """
+SELECT
+    flights.ID,
+    flights.ORIGIN_AIRPORT,
+    flights.DESTINATION_AIRPORT,
+    airlines.AIRLINE,
+    flights.AIRLINE_DELAY AS DELAY
+FROM
+    flights
+    JOIN airlines ON flights.AIRLINE = airlines.ID
+WHERE
+    flights.ID = :flight_id
+;
+"""
+
+
+class FlightData:
+    """
+    The FlightData class is a Data Access Layer (DAL) object that provides an
+    interface to the flight data in the SQLITE database. When the object is
+    created, the class forms connection to the sqlite database file, which
+    remains active until the object is destroyed.
+    """
+
+    def __init__(self, db_uri):
+        """
+        Initialize a new engine using the given database URI
+        """
+        self._engine = create_engine(db_uri)
+
+    def _execute_query(self, query, params):
+        """
+        Execute an SQL query with the params provided in a dictionary,
+        and returns a list of records (dictionary-like objects).
+        If an exception was raised, print the error, and return an empty list.
+        """
+
+        try:
+            with self._engine.connect() as connection:
+                result = connection.execute(text(query), params)
+                return [row for row in result]
+        except Exception as e:
+            print(f"Error executing query: {e}")
+            return []
+
+    def get_flight_by_id(self, flight_id):
+        """
+        Searches for flight details using flight ID.
+        If the flight was found, returns a list with a single record.
+        """
+        params = {"flight_id": flight_id}
+        return self._execute_query(QUERY_FLIGHT_BY_ID, params)
+
+    def __del__(self):
+        """
+        Closes the connection to the databse when the object is to be destroyed
+        """
+        self._engine.dispose()
